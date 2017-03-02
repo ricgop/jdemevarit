682647c6e8cc779f4aa5247cf97aa39c95b98ada
ricgop <richard.gopaul@gmail.com>
2017-03-02 12:44:47 +0100

    changed principle of logout - stay on recipe page

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_start();
-	session_destroy();
-	include 'common/menu.html';
-	echo '<div class="alert alert-success"><strong>Odhlášení</strong> proběhlo úspěšně!</div>'; header( "refresh:2;url=http://localhost/jdemevarit/recepty.php" );
-?>
