f541adccb20f9a170b48da8b229f263e60953061
ricgop <richard.gopaul@gmail.com>
2017-03-09 17:37:54 +0100

    password reset form and email created

--- zapomenute-heslo.php
+++ zapomenute-heslo.php
@@ -24,13 +24,13 @@
         $error_login = true;
       } else {
         try {
-          # check if email is already used
+          # check if email exists
           $email = $_POST["userEmail"];
           $email_query = "SELECT count(*) FROM users WHERE email='$email' AND active='1'";
           $email_result = $dbh->query($email_query)->fetchColumn();
           # if user doesn't exists
           if ($email_result == 0) {
-            $error_general = "Špatně zadaný email nebo heslo!";
+            $error_general = "Email nenalezen!";
             $error_login = true;
           }
         }
@@ -40,67 +40,23 @@
         }
       }
     }
-    # Password Validation
-    if (($_POST["password"] == "") || (!isset($_POST["password"]))) {
-      $error_password = "Heslo nesmí být prázdné!";
-      $error_login = true;
-      $passwd_empty = true;
-    } else {
-      if (strlen($_POST["password"]) > 30) {
-        $error_password = "Heslo nesmí obsahovat víc, než 30 znaků!";
-        $error_login = true;
-      } else {
-        try {
-          if ($error_login == false) {
-            # check if email is already used
-            $password = $_POST["password"];
-            $password_query = "SELECT password FROM user_passwords WHERE email='$email'";
-            $password_result = $dbh->query($password_query)->fetchColumn();
-            # if user doesn't exists
-            if (password_verify($password, $password_result)) {
-            } else {
-              $error_general = "Špatně zadaný email nebo heslo!";
-              $error_login = true;
-            }
-          }
-        }
-        catch (PDOException $exception)
-        {
-          $error_db = true;
-        }
-      }
-    }
-    # login credentials correct - log the user in
-    if ($error_login == false && $error_db == false && (($_POST["password"] != "") || isset($_POST["password"]))) {
+
+    # login credentials correct - generate email
+    if ($error_db == false && $error_login == false) {
       # get user's nickname from db
       try {
-        $email = $_POST["userEmail"];
-        $nick_query = "SELECT username FROM users WHERE email='$email'";
-        $nickname = $dbh->query($nick_query)->fetchColumn();
+        $hash = password_hash($email, PASSWORD_DEFAULT);
+        $pwurl = 'localhost/jdemevarit/reset-hesla.php?=' . $hash;
+        
+        # create & send email
+        $mailbody = 'Tento mail byl vytvořen na základě žádosti o změnu hesla na stránkách www.jdemevarit.cz\nPokud Vám byl zaslán omylem, prosím ignorujte ho.\n\nHeslo resetujete kliknutím na tento link: ' . $pwurl . '\njdemevarit.cz';
+        mail($email, 'Reset hesla - www.jdemevarit.cz', $mailbody);
+        $success = true;
       }
       catch (PDOException $exception)
       {
         $error_db = true;
       }
-      # log the user in
-      session_start();
-      $_SESSION['login_username'] = $nickname; // session initialization with value of PHP variables
-      $_SESSION['login_email'] = $email;
-      try {
-        $limitations_query = "SELECT * FROM user_limitations WHERE email='$email'";
-        $limitations = $dbh->query($limitations_query)->fetchAll();
-        $_SESSION['limitation1'] = $limitations[0]['limitation_1'];
-        $_SESSION['limitation2'] = $limitations[0]['limitation_2'];
-        $_SESSION['limitation3'] = $limitations[0]['limitation_3'];
-        $_SESSION['limitation4'] = $limitations[0]['limitation_4'];
-        $_SESSION['limitation5'] = $limitations[0]['limitation_5'];
-        $_SESSION['limitation6'] = $limitations[0]['limitation_6'];
-      }
-      catch (PDOException $exception)
-      {
-        $error_db = true;
-      }
-      $success = true;
     }
   }
 ?>
@@ -110,7 +66,7 @@
   <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Přihlášení</title>
+    <title>Reset hesla</title>
 
     <link rel="icon" type="image/png" href="images/favicon.png">
 
@@ -157,31 +113,23 @@
     <div class="container-fluid">
       <div id="content">
         <div id="login">
-          <h1>Přihlášení</h1>
-          <?php if($success == true) {echo '<div class="alert alert-success" id="login-success"><strong>Přihlášení</strong> proběhlo úspěšně!</div>'; header( "refresh:2;url=http://localhost/jdemevarit/recepty.php" );}?>
+          <h1>Reset hesla</h1>
+          <p><u>Upozornění</u>: pro dokončení akce potvrďte prosím email.</p>
+          <?php if($success == true) {echo '<div class="alert alert-success" id="login-success"><strong>Reset hesla</strong> proběhl úspěšně!</div>'; header( "refresh:2;url=http://localhost/jdemevarit/recepty.php" );}?>
           <?php if($error_db == true) {echo '<div class="alert alert-danger"><strong>Nastala chyba</strong> - opakujte prosím akci později...</div>';}?>
           <div id="login-container">
             <div class="col-xs-12 col-sm-4">
-              <form id="login-form" method="POST">
-               <div class="<?php if(!isset($error_email) && ((!isset($error_general)) || ($passwd_empty == true))) {echo "form-group";} else {echo "form-group has-error";} ?>" id="form-email">
+              <form id="reset-form" method="POST">
+               <div class="<?php if((!isset($error_general)) && $error_login == false) {echo "form-group";} else {echo "form-group has-error";} ?>" id="form-email">
                  <label for="userEmail">Email</label><span class="star"> *</span>
                  <input class="form-control" id="email" name="userEmail" type="text" placeholder="např. jan.novak@email.cz" value="<?php if(isset($_POST['userEmail'])) echo $_POST['userEmail']; ?>">
                  <div class="error"><?php if(isset($error_email)) echo $error_email; ?></div>
                  <div class="error"><?php if(isset($error_general) && (!isset($error_email)) && (!isset($error_password))) echo $error_general; ?></div>
               </div>
-              <div class="<?php if(!isset($error_password) && (!isset($error_general))) {echo "form-group";} else {echo "form-group has-error";} ?>" id="form-email">
-                <label for="password">Heslo</label><span class="star"> *</span>
-                <input class="form-control" id="password" name="password" type="password" placeholder="heslo" value="<?php if(isset($_POST['password'])) echo $_POST['password']; ?>">
-                <div class="error"><?php if(isset($error_password)) echo $error_password; ?></div>
-                <div class="error"><?php if(isset($error_general) && (!isset($error_email)) && (!isset($error_password))) echo $error_general; ?></div>
-              </div>
               <div>
-                <button type="submit" class="btn btn-primary"">Přihlásit</button>
-
+                <button type="submit" class="btn btn-primary"">Odeslat</button>
               </div>
               </form>
-                            <p></p>
-              <a href="zapomenute-heslo.php" id="forgoten"><u>Zapomněli jste heslo?</u></a>
             </div> <!-- .col-sm-4 -->
           </div> <!-- #login-container -->
         </div> <!-- #login -->
